99419a34d4a666f268b2518c8225b3d54f05d15d
SJ <samuel.jaja21@gmail.com>
2023-06-12 15:22:06 +0100

    Added Post Launches Endpoint with Request Validation

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,5 +1,7 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
     flightNumber: 100,
     mission: "Kepler Exploration X",
@@ -16,6 +18,21 @@ launches.set(launch.flightNumber, launch);
 function getAllLaunches(){
    return Array.from(launches.values())
 }
+
+function addNewLaunch(launch){
+    latestFlightNumber++;
+    launches.set(
+        latestFlightNumber, 
+        Object.assign(launch, {
+        flightNumber: latestFlightNumber,
+        success:true,
+        upcoming: true,
+        customer: ["Zero to Mastery", "NASA"],
+    })
+    );
+}
+
 module.exports = {
     getAllLaunches,
+    addNewLaunch,
 }
